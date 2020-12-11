32681970701f5fc24154dc66d55f634d3d31b76e
Joao Pereira <joaopedrosp@gmail.com>
2020-12-11 12:54:05 +0000

    Improved PEP8 style guide specifications and added argument parsing to flair counting. It is now possible to save monthly counts and retrieve users with a specific flair through command line arguments

--- reddit_utils/flair-count/index.py
+++ reddit_utils/flair-count/index.py
@@ -1,34 +1,40 @@
 from reddit_utils import prepare_dot_env
-import praw
-import os
 from dotenv import load_dotenv
 from pathlib import Path
 from pprint import pprint
-import csv
 from collections import Counter
+import csv
 import re
 import sys
+import argparse
+import praw
+import os
 
 
-def printAllFlairsAssigned(subreddit):
+def get_flair_table_list(subreddit):
     redditors_flairs = [re.sub(r":.*:\s", "", template['flair_text'])
                         for template in subreddit.flair(limit=None)]
 
     redditors_flairs_counter = Counter(redditors_flairs)
     all_elements = redditors_flairs_counter.most_common()
 
-    print('Flair| Count')
-    print('---|---')
+    flair_table_list = list()
+
+    flair_table_list.append('Flair| Count')
+    flair_table_list.append('---|---')
     for flair_count in all_elements:
-        print('{} | {}'.format(flair_count[0], flair_count[1]))
+        flair_table_list.append('{} | {}'.format(
+            flair_count[0], flair_count[1]))
 
-    print()
-    print('Total number of teams with flairs assigned: {}'.format(
+    flair_table_list.append('')
+    flair_table_list.append('Total number of teams with flairs assigned: {}'.format(
         len(redditors_flairs_counter)))
-    print('Total flairs assigned: {}'.format(len(redditors_flairs)))
+    flair_table_list.append(
+        'Total flairs assigned: {}'.format(len(redditors_flairs)))
+    return flair_table_list
 
 
-def printRedditorsFromClub(subreddit, club):
+def print_redditors_from_club(subreddit, club):
     redditors_flairs_club = [(re.sub(r":.*:\s", "", template['flair_text']), template['user'].name)
                              for template in subreddit.flair(limit=None)]
 
@@ -38,8 +44,21 @@ def printRedditorsFromClub(subreddit, club):
     print(', '.join(redditor_names))
 
 
+def save_flair_count(flair_table_list, year, month):
+    count_filename = "{}_{}.txt".format(year, month)
+    counts_filepath = os.path.join(
+        os.getcwd(), '..', 'flair-breakdown', 'counts', count_filename)
+    with open(counts_filepath, mode='w', encoding='utf8') as f:
+        f.writelines("%s\n" % line for line in flair_table_list[:-1])
+        f.write("%s" % flair_table_list[-1])
+
+
 if __name__ == "__main__":
-    flairs_dict = dict()
+    parser = argparse.ArgumentParser(description="Flair counter")
+    parser.add_argument('--team', '-t', dest='team',
+                        type=str, nargs=1, metavar='t')
+    parser.add_argument('--save, -s', dest='save', type=int, nargs=2)
+    args = parser.parse_args()
 
     reddit = praw.Reddit(client_id=os.getenv("REDDIT_APP_ID"),
                          client_secret=os.getenv("REDDIT_APP_SECRET"),
@@ -49,5 +68,14 @@ if __name__ == "__main__":
 
     subreddit = reddit.subreddit("Euroleague")
 
-    printAllFlairsAssigned(subreddit)
-    # printRedditorsFromClub(subreddit, 'Žalgiris Kaunas')
+    try:
+        team_name = args.team[0]
+        print_redditors_from_club(subreddit, team_name)
+    except:
+        flair_table_list = get_flair_table_list(subreddit)
+        print('\n'.join(flair_table_list))
+        if args.save:
+            year = str(args.save[0])
+            month = str(args.save[1])
+
+            save_flair_count(flair_table_list[:-3], year, month)
